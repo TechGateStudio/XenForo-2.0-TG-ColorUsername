81a78524324c7a8bac1a3b284413d592d96574cc
West14 <30056636+West14@users.noreply.github.com>
2019-08-09 16:21:50 +0300

    1.0.0 Alplha 1

--- XF/Template/Templater.php
+++ XF/Template/Templater.php
@@ -14,15 +14,18 @@ class Templater extends XFCP_Templater
 			{
 				if ($user->w_cu_type == 'single')
 				{
-					$html = str_replace('dir', sprintf('style="color: %s;" dir', $user->w_cu_color), $html);
+					$html = str_replace('dir', sprintf('style="color: %s; background: unset;" dir', $user->w_cu_color), $html);
+					$html = str_replace('class="','class="username--singleColor', $html);
 				}
 				if ($user->w_cu_type == 'gradient')
 				{
 					$html = str_replace('dir', sprintf('
+					class="username--gradient"
 					style="background: linear-gradient(to right, %s, %s);
 					-webkit-background-clip: text;
 					-webkit-text-fill-color: transparent;" dir',
 					$user->w_cu_color, $user->w_cu_color_second), $html);
+					$html = str_replace('class="', 'class="username--gradient ', $html);
 				}
 			}
 		}
